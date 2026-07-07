da85df364cbd8d721342bdc5123831be61dd91b2
Deepika4825 <deepikaraj4825@gmail.com>
2026-07-07 18:55:25 +0530

    Convert Super Market to a normal folder

---
+++ Super Market/CardItem.java
@@ -0,0 +1,16 @@
+class CartItem{
+
+    int id;
+    String name;
+    double price;
+    int qty;
+
+    CartItem(int id,String name,double price,int qty){
+
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.qty = qty;
+
+    }
+}
---
+++ Super Market/Main.java
@@ -0,0 +1,9 @@
+class Main{
+
+    public static void main(String[] args){
+
+        Store s = new Store();
+        s.start();
+
+    }
+}
